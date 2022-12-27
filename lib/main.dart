383f91e608f5e5104d014db4bb6028747eedbdc7
sudharshanyadav <sudharshanyadav.iimt@gmail.com>
2022-12-27 12:26:07 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,58 +14,106 @@ class MyApp extends StatelessWidget {
       title: "My Product",
       theme: ThemeData(
         primarySwatch: Colors.cyan,
+        fontFamily: "regular",
+
       ),
-      home: MyHomePage(),
+        home: HomePage(),
     );
   }
 }
-class MyHomePage extends StatefulWidget {
-
-
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String name ="Prince!";
+class _HomePageState extends State<HomePage> {
+
+  int index=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Learning",
-
-        )
+          "My Products",
+        ),
+       actions: [
+         IconButton(
+             onPressed: (){},
+             icon: Icon(Icons.search),
+           ),
+       ],
       ),
-      body: Column(
-        children: [
-          Text(
-            "$name",
-            style: TextStyle(
-              fontSize: 36.0,
-              fontFamily: "proxima",
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.red[200],
+        showUnselectedLabels: true,
+        onTap: (index){
+          setState(() {
+            index= index;
+          });
+
+        },
+        currentIndex: index,
+
+        items: [
+         BottomNavigationBarItem(
+             icon:Icon(
+               Ionicons.home_outline,
+
+             ),
+           label: "Home",
+         ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Ionicons.cart_outline,
             ),
+            label: "Cart",
           ),
-           ElevatedButton(
-               onPressed: (){
-                 name = "Desi programmer";
-                 setState(() {
+          BottomNavigationBarItem(
+              icon: Icon(
+                Ionicons.grid_outline,
+              ),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Ionicons.person_outline,
 
-                 });
-                 print(name);
-               },
-
-               child: Text(
-                 "Change Name",
-               ),
-           )
-
+          ),
+            label: "profile",
+          ),
         ],
       ),
+
+       //  body:buildWidget(),
+      body:ListView(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+                aspectRatio:16/9,
+                 autoPlay:true,
+           ),
+            items: [1,2,3,4,5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                          color: Colors.amber
+                      ),
+                      child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                  );
+                },
+              );
+            }).toList(),
+          )
+      ],) ,
     );
+
+
   }
 }
-
-
 
